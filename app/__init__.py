4fc467cad000ea07d26d248ed3e34f79483055ce
Heavyblade <cristianvg2003@gmailcom>
2025-07-15 21:16:31 -0500

    Mejorando OCR text

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import markdown
+from markupsafe import Markup
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +12,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+@app.template_filter('markdown')
+def markdown_filter(text):
+    """Convert markdown text to HTML"""
+    if not text:
+        return ''
+    return Markup(markdown.markdown(text, extensions=['nl2br', 'codehilite']))
 
 from app import routes, models
